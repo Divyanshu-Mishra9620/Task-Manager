d45469499b1b46a559b62a9f7c2049908aac8b22
student-dv <dvbeast465@gmail.com>
2025-02-21 21:25:36 +0530

    fix issue

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://task-manager-frontend-7dms.vercel.app",
+      "http://task-manager-frontend-7dms.vercel.app",
     ],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
